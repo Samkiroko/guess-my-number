e064f2b6754c2c67b76ba2054c3670104c4377f3
Samkiroko <kirokodev@gmail.com>
2021-02-18 15:08:34 +0300

    Selecting and manipulating elements

--- .history/script_20210218124456.js
+++ .history/script_20210218124456.js
@@ -5,6 +5,3 @@ document.querySelector('.message').textContent = '🎉 Correct Number';
 
 document.querySelector('.number').textContent = 13;
 document.querySelector('.score').textContent = 10;
-
-document.querySelector('.guess').value = 23;
-console.log(document.querySelector('.guess').value);
--- .history/script_20210218124652.js
+++ .history/script_20210218124652.js
@@ -6,5 +6,4 @@ document.querySelector('.message').textContent = '🎉 Correct Number';
 document.querySelector('.number').textContent = 13;
 document.querySelector('.score').textContent = 10;
 
-document.querySelector('.guess').value = 23;
-console.log(document.querySelector('.guess').value);
+document.querySelector('.guess').value;
