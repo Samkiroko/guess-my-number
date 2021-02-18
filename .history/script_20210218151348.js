d20bc64bf80b38f54ee5791b6c770944adb3e133
Samkiroko <kirokodev@gmail.com>
2021-02-18 15:26:03 +0300

    Handling click event

--- .history/script_20210218151348.js
+++ .history/script_20210218151348.js
@@ -11,10 +11,5 @@ console.log(document.querySelector('.guess').value);
 */
 
 document.querySelector('.check').addEventListener('click', () => {
-  const guess = Number(document.querySelector('.guess').value);
-  console.log(guess, typeof guess);
-
-  if (!guess) {
-    document.querySelector('.message').textContent = '⛔️ No number';
-  }
+  console.log(document.querySelector('.guess').value);
 });
--- .history/script_20210218152005.js
+++ .history/script_20210218152005.js
@@ -13,8 +13,4 @@ console.log(document.querySelector('.guess').value);
 document.querySelector('.check').addEventListener('click', () => {
   const guess = Number(document.querySelector('.guess').value);
   console.log(guess, typeof guess);
-
-  if (!guess) {
-    document.querySelector('.message').textContent = '⛔️ No number';
-  }
 });
